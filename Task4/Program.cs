6df26db66b2c3401a5c53b555eb8c06718f6c1c5
Artur <aubakirov-artur@yandex.ru>
2022-09-23 15:56:57 +0700

    Task4 is done

---
+++ Task4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter first value:");
+int v1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second value:");
+int v2 = int.Parse( Console.ReadLine());
+Console.WriteLine("Enter third value:");
+int v3 = int.Parse( Console.ReadLine());
+Console.WriteLine($"Max = {Math.Max(Math.Max(v1, v2),v3)}");
