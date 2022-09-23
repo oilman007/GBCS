8ddb0035a2882720105efd3b5515192f7e5fb150
Artur <aubakirov-artur@yandex.ru>
2022-09-23 16:22:13 +0700

    Task8 is done

---
+++ Task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter value:");
+int n = int.Parse(Console.ReadLine());
+
+string result = string.Empty;
+for(int i=1; i<n; i+=2)
+    result += $"{i+1} ";
+
+Console.WriteLine(result);
