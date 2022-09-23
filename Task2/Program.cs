7d6a0981467157ae25de201551e1a2d3f0a8d631
Artur <aubakirov-artur@yandex.ru>
2022-09-23 15:52:31 +0700

    Task2 is done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter first value:");
+int v1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second value:");
+int v2 = int.Parse( Console.ReadLine());
+Console.WriteLine($"Min = {Math.Min(v1, v2)}, Max = {Math.Max(v2, v2)}");
