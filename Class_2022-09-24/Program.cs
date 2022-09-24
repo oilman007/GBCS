58f0c0380360a94086a0a669e98b563583e36f8b
Artur <aubakirov-artur@yandex.ru>
2022-09-24 15:14:34 +0700

    add new class work

---
+++ Class_2022-09-24/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Enter value:");
+int n = int.Parse(Console.ReadLine());
+
+string result = string.Empty;
+for(int i=0; i<n; i++)
+    result += $"{(i+1)*(i+1)} ";
+
+Console.WriteLine(result);
--- test.cs
+++ test.cs
@@ -1,1 +1,11 @@
-Console.WriteLine("Hello, World");
+Console.Clear();
+Console.Write("Введите облать 1,2,3 или 4: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x == 1)
+    Console.WriteLine("+x,+yI");
+else if (x == 2)
+    Console.WriteLine("+x, -y");
+else if (x == 3)
+    Console.WriteLine("-x, -y");
+else if (x == 4)
+    Console.WriteLine("-x, +y");
