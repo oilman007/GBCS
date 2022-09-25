4b43694a827b3c17a371d6919c838cb6afa9d4e5
Artur <aubakirov-artur@yandex.ru>
2022-09-25 15:26:23 +0700

    add class work

---
+++ Class_2022-09-25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("[");
+Random rand = new Random();
+int[] arr = new int[8];
+for(int i=0; i < 8; ++i)
+{
+    arr[i] = rand.Next(0,2);
+    Console.Write(arr[i] + ",");    
+}
+Console.Write("]");
