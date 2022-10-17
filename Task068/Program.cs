354fe0688e8c8e47ec2b8df704cc771d7fde2c26
Artur <aubakirov-artur@yandex.ru>
2022-10-17 22:55:01 +0700

    task068

--- Task068/Program.cs
+++ Task068/Program.cs
@@ -3,3 +3,21 @@
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 */
+
+
+int A(int m, int n)
+{
+    if(m==0)
+        return n +1;
+    if(n==0)
+        return A(m-1,1);
+    return A(m-1, A(m,n-1));
+}
+
+
+Console.Clear();
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(A(m,n));
