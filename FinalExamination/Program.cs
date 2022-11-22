660968a42fc51889bd41d5a8492361e1a6f6c01d
Artur <aubakirov-artur@yandex.ru>
2022-11-22 19:14:54 +0700

    working case created

--- FinalExamination/Program.cs
+++ FinalExamination/Program.cs
@@ -12,6 +12,17 @@ string values = Console.ReadLine();
 
 string[] split = values.Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
 
+int n=0;
 for(int i =0; i<split.Length; ++i)
     if(split[i].Length <= 3)
-        Console.Write(split[i]) + " ";
+        n++;
+
+string[] result = new string[n];
+
+for(int i =0, j =0; i<split.Length; ++i)
+    if(split[i].Length <= 3)
+        result[j++] = split[i];
+
+
+for(int i =0; i<result.Length; ++i)
+    Console.Write($"{result[i]} ");
